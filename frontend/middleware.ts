ceae6a9bd70ec06aa7cec7b38393469654432179
Mostafa Ghadiri <mostafaghr12345@gmail.com>
2026-07-19 12:47:03 +0330

    prepare to upload to VPS

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,19 +1,80 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// middleware.ts
 
-export function middleware(req: NextRequest) {
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+    const sessionCookie = req.cookies.get('token');
+    const url = req.nextUrl.clone();
+
+
+    const token = req.cookies.get("token")?.value;
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/auth/me`,
+        {
+            headers: {
+                Cookie: `token=${token}`
+            }
+        }
+    );
+    const data = await res.json();
+
+    const hasActiveSubscription = data.user?.hasActiveSubscription
+    const role = data.user?.role
+
+    if (url.pathname === '/subscription' && !sessionCookie) {
+
+        url.pathname = '/login'
+        return NextResponse.redirect(url);
+    }
+
+    if (url.pathname === '/' && sessionCookie || url.pathname === "/login" && sessionCookie || url.pathname === "/register" && sessionCookie) {
+
+        url.pathname = '/home';
+        return NextResponse.redirect(url);
+    }
+    if (url.pathname === '/home' && !sessionCookie) {
+
+        url.pathname = '/login'
+        return NextResponse.redirect(url);
+    }
+
+    if (url.pathname.startsWith('/movies') && !sessionCookie) {
+        url.pathname = '/login';
+        return NextResponse.redirect(url);
+    }
+
+    if (url.pathname === '/account' && !sessionCookie) {
+
+        url.pathname = '/login'
+        return NextResponse.redirect(url);
+    }
+
+    if (url.pathname.startsWith("/admin") && !sessionCookie) {
+
+        url.pathname = '/login'
+        return NextResponse.redirect(url);
+    }
+    if (url.pathname.startsWith("/admin") && role === "user") {
+
+        url.pathname = '/'
+        return NextResponse.redirect(url);
+    }
+
+    if (url.pathname.startsWith('/movies') && !hasActiveSubscription) {
+        url.pathname = '/subscription';
+        return NextResponse.redirect(url);
+    }
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        "/",
-        "/login",
-        "/register",
-        "/subscription",
-        "/home",
-        "/account",
-        "/movies/:path*",
-        "/admin",
-    ],
+    matcher: ['/', '/login', '/register', '/subscription', '/home', '/account', '/movies/:path*', '/admin'],
+
+
+
+
+
 };
+
+
