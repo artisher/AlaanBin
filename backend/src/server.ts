feb2c17dbc73b24689e2c9cad229086dc779a3ac
Mostafa Ghadiri <mostafaghr12345@gmail.com>
2026-08-07 21:17:46 +0330

    feat:improve ux

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -552,9 +552,9 @@ app.post('/api/auth/login', async (req, res) => {
 app.post("/api/auth/logout", (req, res) => {
     res.clearCookie("token", {
         httpOnly: true,
-        secure: true,
+        secure: false,
         sameSite: "lax",
-        domain: ".alanbin.com",
+        // domain: ".alanbin.com",
         path: "/",
     });
 
