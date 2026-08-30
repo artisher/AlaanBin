4861eab91e10413eb16a3d32bbffe58cdb549046
Mostafa Ghadiri <mostafaghr12345@gmail.com>
2026-08-30 12:49:28 +0330

    feat: Add storageAddress input

--- frontend/components/CreateMovieModal.tsx
+++ frontend/components/CreateMovieModal.tsx
@@ -149,7 +149,18 @@ export const CreateMovieModal: React.FC<CreateMovieModalProps> = ({
                         />
                         {errors.rating && <span className="text-red-400 text-xs mt-1">{errors.rating.message}</span>}
                     </div>
+                    {/* url */}
+                    <div className='w-full sm:w-75'>
+                        <label htmlFor="videoUrl" className="block text-sm font-medium text-gray-300 mb-1">ادرس دیتابیس</label>
+                        <input
 
+                            id="videoUrl"
+                            value={storageFilename ? `/video/${storageFilename}` : ""}
+                            readOnly
+                            placeholder="فیلمی از Storage انتخاب نشده"
+                            className="w-full rounded-md border border-gray-600 bg-gray-700 text-white shadow-sm focus:border-[#14c78b] focus:ring-[#14c78b] sm:text-sm p-2"
+                        />
+                    </div>
 
                     {/* نقش */}
                     <div className='w-full sm:w-75'>
