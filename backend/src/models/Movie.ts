9720c89450dd5dcf751cc9b9a14d37b6565b155d
Mostafa Ghadiri <mostafaghr12345@gmail.com>
2026-09-21 13:42:33 +0330

    feat: add hero image to movie schema

--- backend/src/models/Movie.ts
+++ backend/src/models/Movie.ts
@@ -15,7 +15,7 @@ const movieSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-
+  heroImage: String,
   rating: { type: Number, min: 0, max: 10 },
 
   topWeek: { type: Boolean, default: false },
@@ -28,7 +28,7 @@ const movieSchema = new mongoose.Schema({
 
   duration: Number,
 
-  
+
 
 });
 
